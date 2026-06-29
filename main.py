d1a53c34b9de4a884a47b3807a1176d1461d3d3a
shumai053 <raku428@icloud.com>
2026-06-29 15:57:09 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_excel("data.xlsx")
+
+summary = df.describe()
+summary.to_excel("summary.xlsx")
+
+plt.figure(figsize=(8,5))
+df["売上"].plot(kind="line")
+plt.title("売上推移")
+plt.savefig("graph.png")
